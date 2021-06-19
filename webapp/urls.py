f7b30e4670bf8cfca3d055af6953ecf0aa6ee89b
Appuxif <smokost@gmail.com>
2021-06-19 21:06:44 +0700

    add rest_framework to the settings.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import (
     path,
@@ -25,3 +26,34 @@ urlpatterns = [
     path('api/token/auth/', views.obtain_auth_token),
     path('', include('mainapp.urls', namespace='mainapp')),
 ]
+
+if settings.DEBUG:
+    from rest_framework.documentation import (
+        get_docs_view,
+        get_schemajs_view,
+        include_docs_urls,
+    )
+
+    urlpatterns += [
+        # path(
+        #     'api/auth/',
+        #     RedirectView.as_view(url='login/')
+        # ),
+        path(
+            'api/auth/',
+            include('rest_framework.urls', namespace='rest_framework')
+        ),
+        path(
+            'api/docs/',
+            include_docs_urls(title='API Service')
+        ),
+        path(
+            'api/docs_view/',
+            get_docs_view(title='API Service')
+        ),
+        path(
+            'api/schemajs/',
+            get_schemajs_view(title='API Service')
+        ),
+    ]
+
