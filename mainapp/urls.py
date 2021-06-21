b400621d8d51e75521419e43d07c2e3b31497417
Appuxif <smokost@gmail.com>
2021-06-21 11:27:03 +0700

    update mainapp.urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from mainapp.api import (
+    PollAPIView,
+    MemberAnswerAPIView,
+)
+
+router = SimpleRouter()
+router.register('polls', PollAPIView)
+router.register('answers', MemberAnswerAPIView)
 
 app_name = 'mainapp'
-urlpatterns = [
-
-]
+urlpatterns = router.urls
