cc64fa72c40a8480e6c4764945cde06bab97eeb1
Appuxif <smokost@gmail.com>
2021-06-21 11:27:22 +0700

    add signals

--- mainapp/signals.py
+++ mainapp/signals.py
@@ -3,8 +3,20 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+from mainapp.models import (
+    MemberAnswer,
+    Poll,
+)
+
 
 @receiver(post_save, sender=User)
-def post_save_user(sender, instance: User, created, **kwargs):
+def post_save_user(sender, instance, created, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+@receiver(post_save, sender=MemberAnswer)
+def post_save_member_answer(sender, instance: MemberAnswer, created, **kwargs):
+    # Присоединяем участника к опросу, если это его первый ответ
+    poll = Poll.objects.get(question__memberanswer=instance)
+    poll.members.add(instance.member)
